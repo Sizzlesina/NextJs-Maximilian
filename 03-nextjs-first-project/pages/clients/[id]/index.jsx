f920e1c1b7b2bbffc33d076256e5a3b1b5d76ec0
Sizzlesina <www.sins604@gmail.com>
2024-03-26 13:40:19 +0330

    Navigating between routes using a button

--- 03-nextjs-first-project/pages/clients/[id]/index.jsx
+++ 03-nextjs-first-project/pages/clients/[id]/index.jsx
@@ -4,9 +4,27 @@ function ClientProjectsPage() {
   const router = useRouter();
   console.log(router.query);
 
+  function loadProjectHandler() {
+    // load data...
+
+    /*
+    @ Syntax 1:
+     router.push("/clients/max/projecta"); => We can go back after the navigation
+
+     router.replace("/clients/max/projecta"); => We can't go back after the navigation
+     */
+
+    // @ Syntax 2:
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "projecta" },
+    });
+  }
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
