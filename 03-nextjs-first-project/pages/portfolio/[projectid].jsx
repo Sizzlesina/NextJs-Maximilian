14fa3f0300f44bd08a6ed911fd1901182262b2dc
Sizzlesina <www.sins604@gmail.com>
2024-03-26 13:05:27 +0330

    Nested dynamic routes

--- 03-nextjs-first-project/pages/portfolio/[projectid].jsx
+++ 03-nextjs-first-project/pages/portfolio/[projectid].jsx
@@ -1,4 +1,16 @@
+import { useRouter } from "next/router";
+
 function PortfolioProjectPage() {
+  const router = useRouter();
+
+  /*
+  console.log(router.pathname);
+  @ Output: portfolio/[projectid]
+  console.log(router.query);
+  @ Output: An object with the id we entered on the URL
+  ++ To access the route id : router.query.projectid
+*/
+
   return (
     <div>
       <h1>The Portfolio Project Page</h1>
