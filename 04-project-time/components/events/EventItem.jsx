97250ff8502cd9dc55ce541461a591074967781b
Sizzlesina <www.sins604@gmail.com>
2024-03-27 09:38:13 +0330

    Added button component and icons

--- 04-project-time/components/events/EventItem.jsx
+++ 04-project-time/components/events/EventItem.jsx
@@ -1,6 +1,9 @@
-import Link from "next/link";
-import styles from "./EventItem.module.css";
+import Button from "../ui/Button";
+import DateIcon from "../icons/DateIcon";
 
+import styles from "./EventItem.module.css";
+import AddressIcon from "../icons/AddressIcon";
+import ArrowRightIcon from "../icons/ArrowRightIcon";
 function EventItem({ title, image, date, location, id }) {
   const humarnReadableDate = new Date(date).toLocaleDateString("en-US", {
     day: "numeric",
@@ -18,14 +21,21 @@ function EventItem({ title, image, date, location, id }) {
         <div className={styles.summary}>
           <h2>{title}</h2>
           <div className={styles.date}>
+            <DateIcon />
             <time>{humarnReadableDate}</time>
           </div>
         </div>
         <div className={styles.address}>
+          <AddressIcon />
           <address>{formattedAddress}</address>
         </div>
         <div className={styles.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={styles.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
