35421d46921538281189d089d9c09f161cca9e0c
Sizzlesina <www.sins604@gmail.com>
2024-03-27 11:28:28 +0330

    Project finished

--- 04-project-time/pages/index.jsx
+++ 04-project-time/pages/index.jsx
@@ -1,12 +1,17 @@
 import EventList from "../components/events/EventList";
+import ErrorAlert from "../components/ui/ErrorAlert";
 import { getFeaturedEvents } from "../dummy-data";
 
 function HomePage() {
   const featuredEvents = getFeaturedEvents();
-  
+
   return (
     <div>
       <EventList events={featuredEvents} />
+      <ErrorAlert>
+        <p>This Project is DONE </p>
+        <p>(Undo this field if you want to)</p>
+      </ErrorAlert>
     </div>
   );
 }
