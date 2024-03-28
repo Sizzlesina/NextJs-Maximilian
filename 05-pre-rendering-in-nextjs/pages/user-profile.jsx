fa201aa1783afd43c7d95a89fe13e7994811d230
Sizzlesina <www.sins604@gmail.com>
2024-03-28 10:05:17 +0330

    Pre-fetch the dynamic routes using getServerSideProps function (As same as before)

--- 05-pre-rendering-in-nextjs/pages/user-profile.jsx
+++ 05-pre-rendering-in-nextjs/pages/user-profile.jsx
@@ -5,10 +5,7 @@ function UserProfilePage({ username }) {
 export default UserProfilePage;
 
 export async function getServerSideProps(context) {
-  const { params, req, res } = context; // With getServerSideProps function we can have access to requests and responses
-  // @ This built in function is for when we dont need the request and response but we need to ensure that this code will execute for every request
-  // @ Or when we need special header or cookie data
-
+  const { params, req, res } = context;
 
   return {
     props: {
