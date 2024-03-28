1410c60612eb252174542aae1de1d968b7f3dff0
Sizzlesina <www.sins604@gmail.com>
2024-03-28 12:54:54 +0330

    Implementing client side data fetching using useSWR

--- 05-pre-rendering-in-nextjs/pages/last-sales.jsx
+++ 05-pre-rendering-in-nextjs/pages/last-sales.jsx
@@ -1,50 +1,37 @@
 import { useEffect, useState } from "react";
-
+import useSWR from "swr";
 function LastSalesPage() {
   const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(function () {
-    async function getData() {
-      try {
-        // Fetch data
-        setIsLoading(true);
-        const response = await fetch(
-          "https://nextjs-course-434e2-default-rtdb.firebaseio.com/sales.json"
-        );
-        const data = await response.json();
+  // Fetch data in another way from server side AND Format the form of data
+  // @ Second parameter of the useSWR is a fetcher function which in it we must first fetch the data in the parameters of the function then do whatever we want to the data
+  const { data, error } = useSWR(
+    "https://nextjs-course-434e2-default-rtdb.firebaseio.com/sales.json",
+    async function getData(url) {
+      const response = await fetch(url);
+      const data = await response.json();
 
-        // Transform the data in a Javascript form to use in the application
-        const transfromedSales = [];
-        for (const key in data) {
-          transfromedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        // Store the data to show in the application
-        setSales(transfromedSales);
-        setIsLoading(false);
-      } catch (err) {
-        // Error handling in case we dont get the data
-        console.error(err.message);
-        throw new Error("Failed to fetch data!");
+      const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
       }
+      setSales(transformedSales);
+      return data;
     }
+  );
 
-    // Calling the function
-    getData();
-  }, []);
-
-  // Loading handler
-  if (isLoading) {
-    return <p>Loading...</p>;
+  // Error handling (If fail fetching data)
+  if (error) {
+    return <p>No data yet...</p>;
   }
 
-  // In case we dont have the data (This piece of code will be pre-rendered by nextJS)
-  if (!sales) {
-    return <p>No data yet...</p>;
+  // If the data is not exist yet or the render's not finished
+  if (!data || !sales) {
+    return <p>Loading...</p>;
   }
 
   // Main code
