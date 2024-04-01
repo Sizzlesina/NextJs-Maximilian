8efb934ff285cf5102a796f0bab4672b15af6109
Sizzlesina <www.sins604@gmail.com>
2024-04-01 17:44:36 +0330

    Render all events server side

--- 04-project-time/pages/events/index.jsx
+++ 04-project-time/pages/events/index.jsx
@@ -1,12 +1,10 @@
 import { useRouter } from "next/router";
 import EventList from "../../components/events/EventList";
 import EventSearch from "../../components/events/EventSearch";
-import { getAllEvents } from "../../dummy-data";
+import { dummyGetAllEvents, getAllEvents } from "../../dummy-data";
 
-function AllEventsPage() {
-  const events = getAllEvents();
+function AllEventsPage({ events }) {
   const router = useRouter();
-
   function handleFindEvents(year, month) {
     const fullPath = `/events/${year}/${month}`;
 
@@ -19,6 +17,27 @@ function AllEventsPage() {
       <EventList events={events} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  try {
+    const events = await getAllEvents();
+    return {
+      props: {
+        events: events,
+      },
+      revalidate: 60,
+    };
+  } catch (err) {
+    console.error(err.message);
+    const events = dummyGetAllEvents();
+    return {
+      props: {
+        events: events,
+      },
+      revalidate: 60,
+    };
+  }
 }
 
 export default AllEventsPage;
