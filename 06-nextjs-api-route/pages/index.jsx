42d3901459d8e1ab85ec2c9f289beebbd95a584c
Sizzlesina <www.sins604@gmail.com>
2024-04-02 13:44:35 +0330

    Added a api route and the logic inside

--- 06-nextjs-api-route/pages/index.jsx
+++ 06-nextjs-api-route/pages/index.jsx
@@ -1,7 +1,36 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailInput = useRef();
+  const feedbackInput = useRef();
+
+  function handleSubmitForm(e) {
+    e.preventDefault();
+
+    const enteredEmail = emailInput.current.value;
+    const enteredFeedback = feedbackInput.current.value;
+
+    fetch()
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form>
+        <div>
+          <label htmlFor='email'>Your Email Address</label>
+          <input type='email' id='email' ref={emailInput} />
+        </div>
+        <div>
+          <label htmlFor='feedback'>Your Feedback</label>
+          <textarea
+            name='feedback'
+            id='feedback'
+            rows='5'
+            ref={feedbackInput}></textarea>
+        </div>
+        <button type='submit'>Send Feedback</button>
+      </form>
     </div>
   );
 }
