cfef589cd1bb8f483bd86000f1658adf5b39db51
Sizzlesina <www.sins604@gmail.com>
2024-04-02 10:49:41 +0330

    Add title and description to the homepage (In the head section of the html file)

--- 04-project-time/pages/index.jsx
+++ 04-project-time/pages/index.jsx
@@ -1,15 +1,29 @@
+// Next js imports
+import Head from "next/head";
+
+// Component imports
 import EventList from "../components/events/EventList";
+
+// Handler function imports
 import { dummyGetFeaturedEvents } from "../dummy-data";
 import { getFeaturedEvents } from "../helpers/api-util";
 
 function HomePage({ events }) {
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name='description'
+          content='Find a lot of great events that allow you to evolve...'
+        />
+      </Head>
       <EventList events={events} />
     </div>
   );
 }
 
+// Pre-fetching the data
 export async function getStaticProps() {
   try {
     const featuredEvents = await getFeaturedEvents();
