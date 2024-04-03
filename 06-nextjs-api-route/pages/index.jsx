85f47eb32ec62c8c1c6a41dcb06a5baa0ebae751
Sizzlesina <www.sins604@gmail.com>
2024-04-03 10:14:22 +0330

    send post requests and get requests into an api route and load the data or set the data into an json file

--- 06-nextjs-api-route/pages/index.jsx
+++ 06-nextjs-api-route/pages/index.jsx
@@ -1,6 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function HomePage() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+
   const emailInput = useRef();
   const feedbackInput = useRef();
 
@@ -10,13 +12,31 @@ function HomePage() {
     const enteredEmail = emailInput.current.value;
     const enteredFeedback = feedbackInput.current.value;
 
-    fetch()
+    const reqBody = { email: enteredEmail, text: enteredFeedback };
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
+  function handleLoadFeedback() {
+    fetch("/api/feedback")
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackItems(data.feedback);
+      });
   }
 
   return (
     <div>
       <h1>The Home Page</h1>
-      <form>
+      <form onSubmit={handleSubmitForm}>
         <div>
           <label htmlFor='email'>Your Email Address</label>
           <input type='email' id='email' ref={emailInput} />
@@ -31,6 +51,13 @@ function HomePage() {
         </div>
         <button type='submit'>Send Feedback</button>
       </form>
+      <hr />
+      <button onClick={handleLoadFeedback}>Load Feedback</button>
+      <ul>
+        {feedbackItems.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
