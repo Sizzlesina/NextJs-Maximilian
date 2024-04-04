155c9e0112df2314cd268e9939aaded0af87611f
Sizzlesina <www.sins604@gmail.com>
2024-04-04 11:00:47 +0330

    Notification component added

---
+++ 04-project-time/components/ui/Notification.jsx
@@ -0,0 +1,28 @@
+import styles from "./Notification.module.css";
+
+function Notification(title, message, status) {
+  let statusClasses = "";
+
+  if (status === "success") {
+    statusClasses = styles.success;
+  }
+
+  if (status === "error") {
+    statusClasses = styles.error;
+  }
+
+  if (status === "pending") {
+    statusClasses = styles.pending;
+  }
+
+  const activeClasses = `${styles.notification} ${statusClasses}`;
+
+  return (
+    <div className={activeClasses}>
+      <h2>{title}</h2>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default Notification;
