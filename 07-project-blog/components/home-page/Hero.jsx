0ac9bfa461e6208ef5e31e3e942b181f73b9361b
Sizzlesina <www.sins604@gmail.com>
2024-04-04 13:43:29 +0330

    Adding some content to Hero component and some layouts

--- 07-project-blog/components/home-page/Hero.jsx
+++ 07-project-blog/components/home-page/Hero.jsx
@@ -1,9 +1,21 @@
+import styles from "./Hero.module.css";
+import Image from "next/image";
+
 function Hero() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section className={styles.hero}>
+      <div className={styles.image}>
+        <Image
+          src='/images/site/sina.png'
+          alt='An image showing sina'
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, Im Sina</h1>
+      <p>I am a passionate frontend developer - currently learning Next JS 🎓</p>
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
