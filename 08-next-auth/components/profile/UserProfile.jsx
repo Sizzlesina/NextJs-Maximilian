f56103944c747f113cbf13218af5f905680039a6
Sizzlesina <www.sins604@gmail.com>
2024-04-08 12:43:19 +0330

    Add route protection

--- 08-next-auth/components/profile/UserProfile.jsx
+++ 08-next-auth/components/profile/UserProfile.jsx
@@ -1,11 +1,28 @@
+// Next js imports
+import { useSession, getSession } from "next-auth/client";
 // Component imports
 import ProfileForm from "./ProfileForm";
 // CSS module import
 import styles from "./UserProfile.module.css";
+import { useEffect, useState } from "react";
 
 function UserProfile() {
   // Redirect away if NOT auth
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(function () {
+    getSession().then((session) => {
+      if (!session) {
+        window.location.href = "/auth";
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, []);
+
+  if (isLoading) {
+    return <p className={styles.profile}>Loading...</p>;
+  }
   return (
     <section className={styles.profile}>
       <h1>Your User Profile</h1>
@@ -15,4 +32,3 @@ function UserProfile() {
 }
 
 export default UserProfile;
- 
