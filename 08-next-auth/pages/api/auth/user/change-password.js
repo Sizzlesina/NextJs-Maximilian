379ba751f8e6d547707a3565168ee53396c7b164
Sizzlesina <www.sins604@gmail.com>
2024-04-08 13:50:21 +0330

    Implementing the change password server side logic

--- 08-next-auth/pages/api/auth/user/change-password.js
+++ 08-next-auth/pages/api/auth/user/change-password.js
@@ -1,4 +1,6 @@
 import { getSession } from "next-auth/client";
+import { connectToDatabase } from "../../../../lib/db-util";
+import { hashPassword, verifyPassword } from "../../../../lib/auth";
 
 export default async function handler(req, res) {
   if (req.method !== "PATCH") {
@@ -12,4 +14,45 @@ export default async function handler(req, res) {
     res.status(401).json({ message: "Not authenticated!" });
     return;
   }
+
+  // Extracting the data
+  const userEmail = session.user.email;
+  const oldPassword = req.body.oldPassword;
+  const newPassword = req.body.newPassword;
+
+  // Connecting to the database
+  const client = await connectToDatabase();
+  const usersCollection = client
+    .db(process.env.mongodb_database)
+    .collection("users");
+
+  // Check the user credentials in the database
+  const user = await usersCollection.findOne({ email: userEmail });
+
+  if (!user) {
+    res.status(404).json({ message: "User not found." });
+    client.close();
+    return;
+  }
+  // Get the current password of the user from the database
+  const currentPassword = user.password;
+  // Check if the current password is equal to the old password
+  const passwordsAreEqual = await verifyPassword(oldPassword, currentPassword);
+  if (!passwordsAreEqual) {
+    res.status(422).json({ message: "Invalid password!" });
+    client.close();
+    return;
+  }
+
+  // Hash the new password
+  const hashedPassword = await hashPassword(newPassword);
+
+  // If the two passwords are equal then do the change password operation
+  const result = await usersCollection.updateOne(
+    { email: userEmail },
+    { $set: { password: hashedPassword } }
+  );
+
+  client.close();
+  res.status(200).json({ message: "Password updated!" });
 }
