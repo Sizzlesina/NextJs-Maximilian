6c8b90e39e4af05729d88681bbc24bb52c7a7c4b
Sizzlesina <www.sins604@gmail.com>
2024-04-08 14:10:25 +0330

    Done implementing logic to change the password and send the request on front side

--- 08-next-auth/components/profile/ProfileForm.jsx
+++ 08-next-auth/components/profile/ProfileForm.jsx
@@ -1,18 +1,34 @@
 // CSS module import
+import { useRef } from "react";
 import styles from "./ProfileForm.module.css";
-function ProfileForm() {
+function ProfileForm({ onChangePassword }) {
+  const oldPasswordRef = useRef();
+  const newPasswordRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredOldPassword = oldPasswordRef.current.value;
+    const enteredNewPassword = newPasswordRef.current.value;
+
+    //optional : Client side validations
+    onChangePassword({
+      oldPassword: enteredOldPassword,
+      newPassword: enteredNewPassword,
+    });
+  }
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <div className={styles.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' ref={newPasswordRef} />
       </div>
       <div className={styles.control}>
         <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
+        <input type='password' id='old-password' ref={oldPasswordRef} />
       </div>
       <div className={styles.action}>
-        <button>Change Password</button>
+        <button type='submit'>Change Password</button>
       </div>
     </form>
   );
